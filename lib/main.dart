8f897c746abea7d81fa619e7b05e7168569cdd91
César Machado <cesarmach19@gmail.com>
2021-06-14 18:09:41 -0300

    Stateles widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Bem Vindos'),
-      centerTitle: true,
-      backgroundColor: Colors.purple[600],
-    ),
-    body: Center(
-      child: Text(
-          'Fala Dev',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-        ),
-      ),
-    ),
-    floatingActionButton:  FloatingActionButton(
-      onPressed: () {},
-      child: const Icon(Icons.thumb_up),
-      backgroundColor: Colors.purple[500],
-    ),
-  ),
+  home: Home()
 ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bem Vindos'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[600],
+      ),
+      body: Center(
+        child: Text(
+          'Fala Dev',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+      floatingActionButton:  FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.thumb_up),
+        backgroundColor: Colors.purple[500],
+      ),
+    );
   }
 }
 
