d284c788b5212dff55d27660d879ac61da65609d
César Machado <cesarmach19@gmail.com>
2021-06-15 09:52:12 -0300

    Aprendendo o basico

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -13,26 +14,46 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.purple[600],
       ),
-      body: Center(
-        child: Text(
-          'Fala Dev',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower'
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+              child: Image.asset('assets/foto-1.jpg'),
           ),
-        ),
+          Expanded(
+            flex: 1,
+            child: Container (
+            padding: EdgeInsets.all(30.0),
+            color: Colors.cyan,
+            child: Text('1'),
+            )
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.amber,
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              color: Colors.greenAccent,
+              child: Text('3'),
+            ),
+          )
+        ],
       ),
       floatingActionButton:  FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.thumb_up),
         backgroundColor: Colors.purple[500],
       ),
+
     );
   }
 }
-
 
 
